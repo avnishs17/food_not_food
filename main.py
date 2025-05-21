69d7815c3f55b96ebe78cc6a98b3bef97c53eb9c
Avnish Singh <avnish1708@gmail.com>
2025-05-21 12:39:31 +0530

    Training added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classifier import logger
 from classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline 
 from classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Preparation Stage"
 
@@ -20,6 +21,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     prepare_base_model = PrepareBaseModelTrainingPipeline()
     prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
